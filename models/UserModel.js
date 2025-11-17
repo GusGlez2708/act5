80201e43f76ed0814d48af98e0240e34c595c5cb
GusGlez2708 <gonzalezrosasgustavo27@gmail.com>
2025-11-17 17:30:49 -0600

    x

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,78 +1,79 @@
-const { DataTypes } = require('sequelize');
-const { connection } = require('../config.db');
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        perfil_id: { // This field will now just be a BIGINT without a foreign key reference
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        nombre: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        apellidos: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        nick: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        correo: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            unique: true
+        },
+        contraseña: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        activo: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        },
+        UserAlta: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            defaultValue: "Admin"
+        },
+        FechaAlta: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        UserMod: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            defaultValue: ""
+        },
+        FechaMod: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        UserBaja: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            defaultValue: ""
+        },
+        FechaBaja: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        tableName: 'users',
+        timestamps: true
+    });
 
-const User = connection.define('user', {
-    id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    perfil_id: { // This field will now just be a BIGINT without a foreign key reference
-        type: DataTypes.BIGINT,
-        allowNull: false
-    },
-    nombre: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-    },
-    apellidos: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-    },
-    nick: {
-        type: DataTypes.STRING(20),
-        allowNull: false
-    },
-    correo: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        unique: true
-    },
-    contraseña: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    activo: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
-    },
-    UserAlta: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-        defaultValue: "Admin"
-    },
-    FechaAlta: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-    },
-    UserMod: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-        defaultValue: ""
-    },
-    FechaMod: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-    },
-    UserBaja: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-        defaultValue: ""
-    },
-    FechaBaja: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-    }
-}, {
-    tableName: 'users',
-    timestamps: true
-});
+    User.associate = function(models) {
+        // No associations to Profile as it has been removed.
+    };
 
-// No associations to Profile as it has been removed.
-
-module.exports = { User };
+    return User;
+};
