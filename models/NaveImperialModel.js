5509e6ceab783d7429dcd4febf55083f9a970687
GusGlez2708 <gonzalezrosasgustavo27@gmail.com>
2025-11-17 14:54:56 -0600

    correcion

--- models/NaveImperialModel.js
+++ models/NaveImperialModel.js
@@ -1,32 +1,45 @@
+/**
+ * CORRECCIÓN:
+ * Se cambia la exportación para que sea una función que acepte (sequelize, DataTypes),
+ * siguiendo el mismo patrón que tus otros modelos (UserModel, CategoryModel, etc.).
+ * Se elimina la importación directa de '../config.db' porque 'sequelize' (la conexión)
+ * será inyectada por 'models/index.js'.
+ */
 const { DataTypes } = require('sequelize');
-const { connection } = require('../config.db');
 
-const NaveImperial = connection.define('NaveImperial', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  nombre: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  clase: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  tripulacion: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  estado_operativo: {
-    type: DataTypes.ENUM('activo', 'en_reparacion', 'destruido'),
-    allowNull: false,
-    defaultValue: 'activo',
-  },
-}, {
-  tableName: 'naves_imperiales',
-  timestamps: true,
-});
+module.exports = (sequelize, DataTypes) => {
+  const NaveImperial = sequelize.define('NaveImperial', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    clase: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    tripulacion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    estado_operativo: {
+      type: DataTypes.ENUM('activo', 'en_reparacion', 'destruido'),
+      allowNull: false,
+      defaultValue: 'activo',
+    },
+  }, {
+    tableName: 'naves_imperiales',
+    timestamps: true,
+  });
 
-module.exports = { NaveImperial };
+  // Aquí podrías definir asociaciones en el futuro si quisieras
+  // NaveImperial.associate = (models) => {
+  //   ...
+  // };
+
+  return NaveImperial;
+};
--- models/index.js
+++ models/index.js
@@ -1,17 +1,33 @@
-// Importar todos los modelos para establecer relaciones
-const { Profile } = require('./ProfileModel');
-const { State } = require('./StateModel');
-const { Category } = require('./CategoryModel');
-const { User } = require('./UserModel');
-const { New } = require('./NewModel');
-const { NaveImperial } = require('./NaveImperialModel');
+/**
+ * CORRECCIÓN:
+ * Se restaura el 'models/index.js' original de tu proyecto y SE AÑADE
+ * la línea para cargar el nuevo modelo 'NaveImperialModel'.
+ * Este archivo es el que realmente exporta el objeto 'db' que usan
+ * todos los servicios.
+ */
+const { Sequelize, DataTypes } = require('sequelize');
+const { connection } = require('../config.db'); // Tu conexión principal
+const db = {};
 
-// Exportar todos los modelos
-module.exports = {
-    Profile,
-    State,
-    Category,
-    User,
-    New,
-    NaveImperial
-};
+// Importar modelos existentes
+db.Profile = require('./ProfileModel')(connection, DataTypes);
+db.State = require('./StateModel')(connection, DataTypes);
+db.Category = require('./CategoryModel')(connection, DataTypes);
+db.User = require('./UserModel')(connection, DataTypes);
+db.New = require('./NewModel')(connection, DataTypes);
+
+// --- AÑADIR ESTA LÍNEA PARA EL NUEVO MODELO ---
+db.NaveImperial = require('./NaveImperialModel')(connection, DataTypes);
+// ------------------------------------------
+
+// Definir asociaciones
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.Sequelize = Sequelize;
+db.connection = connection;
+
+module.exports = db;
