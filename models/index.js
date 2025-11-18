db4845a87655f9a2c1e90d96d8006231d9fbc1f6
GusGlez2708 <gonzalezrosasgustavo27@gmail.com>
2025-11-17 18:30:03 -0600

    aaaaaaaaaaaaa

--- models/index.js
+++ models/index.js
@@ -4,16 +4,13 @@ const { connection } = require('../config.db'); // Importar la conexión configu
 
 const db = {};
 
-// Cargar los modelos que SÍ existen en la DB
-db.Profile = require('./ProfileModel')(connection, DataTypes); // Modelo de Perfil (requerido por User)
-db.User = require('./UserModel')(connection, DataTypes);     // Modelo de Usuario (requerido por Auth)
-
-// Cargar los nuevos modelos
+// Cargar los únicos modelos que nos importan
+db.User = require('./UserModel')(connection, DataTypes);
 db.NaveImperial = require('./NaveImperialModel')(connection, DataTypes);
 db.Emperador = require('./EmperadorModel')(connection, DataTypes);
 db.Mision = require('./MisionModel')(connection, DataTypes);
 
-// Ejecutar asociaciones
+// Ejecutar asociaciones si existen
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
